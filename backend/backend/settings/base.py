6342505550bbf64835930683fd19d80e4a666f79
Middi <richard@richardmiddleton.me>
2020-02-06 15:45:52 +0000

    add vue and cors

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,8 @@ MIDDLEWARE = [
 
     'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -164,3 +167,5 @@ WAGTAIL_SITE_NAME = "backend"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+CORS_ORIGIN_ALLOW_ALL = True
