ccdfbab819c3bf62134610c20acaec206c82a1fe
Middi <richard@richardmiddleton.me>
2020-02-07 13:13:33 +0000

    streamfield frontend working

--- backend/news/models.py
+++ backend/news/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 from wagtail.api import APIField
+
+from wagtail.core import blocks
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import  FieldPanel
+
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.images.blocks import ImageChooserBlock
+
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.images.api.fields import ImageRenditionField
 
@@ -11,7 +18,12 @@ from wagtail.images.api.fields import ImageRenditionField
 
 class NewsPage(Page):
     intro = models.CharField(max_length=250)
-    body = RichTextField(blank=True)
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
+
     image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -20,8 +32,8 @@ class NewsPage(Page):
     ) 
     content_panels = Page.content_panels + [
         FieldPanel('intro'),
-        FieldPanel('body'),
         ImageChooserPanel('image'),
+        StreamFieldPanel('body'),
     ]
 
     api_fields = [
